9da8fdbc81b051bfdff1d29104daf3e7030dd01e
Naveen B <naveenb.javadev@gmail.com>
2026-02-06 20:24:28 +0530

    Add javascript dom files

---
+++ javascript/dom/index.js
@@ -0,0 +1,13 @@
+alert("Welcome to my webpage!");
+alert(greet());
+
+const myName = "John Doe";
+const myAge = 30;
+
+function greet() {
+  console.log(
+    "Hello, my name is " + myName + " and I am " + myAge + " years old."
+  );
+}
+
+greet();
